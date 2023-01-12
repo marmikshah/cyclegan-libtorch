b4993b9b76bd431a81fde420de897d7f3a09ef0a
Marmik Shah <marmikshah@icloud.com>
2023-01-12 23:19:00 +0800

    Moved Generator and Discriminator to namespace

--- include/globals.hpp
+++ include/globals.hpp
@@ -5,4 +5,24 @@
 
 inline torch::Device device = torch::cuda::is_available() ? torch::Device(torch::kCUDA, 0) : torch::Device(torch::kCPU);
 
+inline torch::nn::Conv2dOptions getConv2dOptions(int inChannels, int outChannels, int kernel, int stride, int padding,
+                                                 bool bias = true) {
+  return torch::nn::Conv2dOptions(inChannels, outChannels, torch::ExpandingArray<2>(kernel))
+      .stride(stride)
+      .padding(padding)
+      .bias(bias);
+}
+
+inline torch::nn::ConvTranspose2dOptions getConvTranspose2dOptions(int inChannels, int outChannels, int kernel,
+                                                                   int stride, int padding) {
+  return torch::nn::ConvTranspose2dOptions(inChannels, outChannels, torch::ExpandingArray<2>(kernel))
+      .stride(stride)
+      .padding(padding);
+}
+
+inline torch::nn::ConvTranspose2d createConvTranspose2d(int inChannels, int outChannels, int kernel, int stride,
+                                                        int padding) {
+  return torch::nn::ConvTranspose2d(getConvTranspose2dOptions(inChannels, outChannels, kernel, stride, padding));
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
-#include <torch/torch.h>
-
 #include <iostream>
 
-#include "generator.hpp"
+#include "cyclegan.hpp"
 #include "globals.hpp"
 
 int main() {
   torch::Tensor tensor = torch::rand({1, 3, 224, 224}).to(device);
 
-  std::shared_ptr gen1 = std::make_shared<Generator>();
+  std::shared_ptr gen1 = std::make_shared<CycleGAN::Generator>();
   gen1->to(device);
-  std::cout << gen1->forward(tensor) << std::endl;
+
+  std::shared_ptr gen2 = std::make_shared<CycleGAN::Generator>();
+  gen2->to(device);
+
+  torch::Tensor result = gen1->forward(tensor);
+  std::cout << CycleGAN::generatorLoss(result) << std::endl;
 }
