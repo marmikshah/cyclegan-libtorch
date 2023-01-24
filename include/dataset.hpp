fe99c9a9a3579a3998ea1dcefbbfb907b243034c
Marmik Shah <marmikshah@icloud.com>
2023-01-24 11:08:54 +0800

    Normalize via reference to avoid memory copy

--- include/dataset.hpp
+++ include/dataset.hpp
@@ -1,8 +1,6 @@
 #ifndef ARTIUM_DATASET_HPP
 #define ARTIUM_DATASET_HPP
 
-#include <torchvision/vision.h>
-
 #include <filesystem>
 #include <opencv2/opencv.hpp>
 
@@ -10,20 +8,12 @@
 
 namespace fs = std::filesystem;
 
-torch::Tensor normalize_tensor(torch::Tensor tensor) {
-  auto mean1 = torch::tensor({0.5}).repeat({256, 256});
-  auto mean2 = torch::tensor({0.5}).repeat({256, 256});
-  auto mean3 = torch::tensor({0.5}).repeat({256, 256});
-  auto mean = torch::stack({mean1, mean2, mean3});
-
-  auto std1 = torch::tensor({0.5}).repeat({256, 256});
-  auto std2 = torch::tensor({0.5}).repeat({256, 256});
-  auto std3 = torch::tensor({0.5}).repeat({256, 256});
-  auto std = torch::stack({std1, std2, std3});
+void normalize(torch::Tensor& tensor) {
+  torch::Tensor mean = torch::ones({3, 256, 256}) * 0.5;
+  torch::Tensor std = torch::ones({3, 256, 256}) * 0.5;
 
   tensor = tensor / 255.0;
   tensor = (tensor - mean) / std;
-  return tensor;
 }
 
 class ImageDataset : public torch::data::datasets::Dataset<ImageDataset> {
@@ -35,7 +25,7 @@ class ImageDataset : public torch::data::datasets::Dataset<ImageDataset> {
     // TODO: Get path from command line
     std::string path = "../data/";
 
-    for (const auto &entry : fs::directory_iterator(path)) {
+    for (const auto& entry : fs::directory_iterator(path)) {
       images.push_back(entry.path().string());
     }
     std::cout << "Found " << images.size() << " images";
@@ -46,9 +36,9 @@ class ImageDataset : public torch::data::datasets::Dataset<ImageDataset> {
     cv::resize(image, image, cv::Size(256, 256), 0, 0, 1);
 
     torch::Tensor tensor = torch::from_blob(image.data, {image.rows, image.cols, image.channels()}, at::kByte);
-    tensor = tensor.permute({2, 0, 1});
-    tensor = normalize_tensor(tensor);
 
+    tensor = tensor.permute({2, 0, 1});
+    normalize(tensor);
     torch::Tensor label = torch::full({1}, 1);
 
     return {tensor.to(device), label.to(device)};
