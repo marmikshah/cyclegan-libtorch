79ac32dd156638af378b0537338a2052178ad0fe
Marmik Shah <marmikshah@icloud.com>
2023-01-24 17:43:18 +0800

    Added cxxopts in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
+#include <cxxopts.hpp>
 #include <iostream>
+#include "train.hpp"
 
-#include "cyclegan.hpp"
-#include "globals.hpp"
+int main(int argc, char** argv) {
+  using namespace cxxopts;
+  Options options("Artium", "Style transfer");
 
-int main() {
-  torch::Tensor tensor = torch::rand({1, 3, 224, 224}).to(device);
+  const std::string group = "Artium";
 
-  std::shared_ptr gen1 = std::make_shared<CycleGAN::Generator>();
-  gen1->to(device);
+  options.add_option(group, {"test", "Will perform inference if set to true", value<bool>()->default_value("false")});
 
-  std::shared_ptr gen2 = std::make_shared<CycleGAN::Generator>();
-  gen2->to(device);
+  options.add_option(group, {"dataset", "Path to dataset", value<std::string>()});
 
-  torch::Tensor result = gen1->forward(tensor);
-  std::cout << CycleGAN::generatorLoss(result) << std::endl;
+  // Model Options
+  options.add_option(group, {"width", "Model input width", value<int>()->default_value("256")});
+  options.add_option(group, {"height", "Model input height", value<int>()->default_value("256")});
+  options.add_option(group, {"r,blocks", "Num resnet blocks", value<int>()->default_value("6")});
+
+  // Training Options
+  options.add_option(group, {"e,epochs", "Total epochs for training", value<int>()->default_value("500")});
+  options.add_option(group, {"l,learning-rate", "Learning rate", value<double>()->default_value("0.0005")});
+  options.add_option(group, {"lambda-identity", "Identity loss", value<double>()->default_value("0.5")});
+  options.add_option(group, {"lambda-a", "Identity loss A", value<double>()->default_value("10.0")});
+  options.add_option(group, {"lambda-b", "Identity loss b", value<double>()->default_value("10.0")});
+
+  // Export Options
+  options.add_option(group, {"export-dir", "Export training results to", value<std::string>()->default_value("./")});
+
+  auto result = options.parse(argc, argv);
+  train(result);
 }
