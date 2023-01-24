4573c12fa23e304e80ecb436e65cf04fa1c7b8fa
Marmik Shah <marmikshah@icloud.com>
2023-01-24 11:09:33 +0800

    Removed saveImage from globals

--- include/globals.hpp
+++ include/globals.hpp
@@ -2,7 +2,6 @@
 #define ARTIUM_UTILS_HPP
 
 #include <torch/torch.h>
-#include <opencv2/opencv.hpp>
 
 
 inline torch::Device device = torch::cuda::is_available() ? torch::Device(torch::kCUDA, 0) : torch::Device(torch::kCPU);
@@ -10,16 +9,9 @@ inline torch::Device device = torch::cuda::is_available() ? torch::Device(torch:
 inline int min(int a, int b) { return a < b ? a : b; }
 
 void setGrad(torch::nn::Module& model, bool grad) {
-    for(auto& param: model.parameters()){
-        param.set_requires_grad(grad);
-    }
-}
-
-bool saveImage(torch::Tensor tensor, std::string imagePath) {
-    tensor = tensor.detach().permute({1, 2, 0}).contiguous();
-    tensor = tensor.mul(255).clamp(0, 255).to(torch::kU8).to(torch::kCPU);
-    cv::Mat output(256, 256, CV_8UC3, tensor.data_ptr<uchar>());
-    return cv::imwrite(imagePath, output);
+  for (auto& param : model.parameters()) {
+    param.set_requires_grad(grad);
+  }
 }
 
 #endif
